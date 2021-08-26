fb1454b73e773a66c8414ea5bdd21e50b2be955f
Harshaprabath <prabhathhemasir@gmail.com>
2021-08-26 11:43:59 +0530

    second comit

--- app/src/main/java/com/example/lab5/MainActivity.java
+++ app/src/main/java/com/example/lab5/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.media.MediaPlayer;
 import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +16,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MediaPlayer player = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI);
-        player.setLooping(true);
-        player.start();
+        // service ekata gatta nisa
+        // MediaPlayer player = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI);
+        // player.setLooping(true);
+        // player.start();
 
+        Button btn_start = (Button) findViewById(R.id.button);
+        Button btn_stop = (Button) findViewById(R.id.button2);
+
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this, MyServices.class));
+            }
+        });
+
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this, MyServices.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/lab5/MyServices.java
+++ app/src/main/java/com/example/lab5/MyServices.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
 
+import android.os.IBinder;
 import android.provider.Settings;
 
 import androidx.annotation.Nullable;
@@ -12,11 +13,16 @@ public class MyServices extends Service {
 
     private MediaPlayer player;
 
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        return super.onStartCommand(intent, flags, startId);
 
-        player = MediaPlayer.create(this,Settings.System.DEFAULT_NOTIFICATION_URI);
+        player = MediaPlayer.create(this,Settings.System.DEFAULT_RINGTONE_URI);
         player.setLooping(true);
         player.start();
         return START_STICKY;
@@ -25,11 +31,9 @@ public class MyServices extends Service {
 
     @Override
     public void onDestroy() {
-        super.onDestroy();
+
         super.onDestroy();
         player.stop();
 
     }
-
-
 }
